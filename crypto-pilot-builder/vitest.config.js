6f444205a1aad65bd910bbef1884b1fb698a3042
DEMEUSY <aurelien.demeusy@epitech.eu>
2025-06-10 22:32:56 +0200

    [FIX] requiremnt.txt

--- crypto-pilot-builder/vitest.config.js
+++ crypto-pilot-builder/vitest.config.js
@@ -2,9 +2,9 @@ import { defineConfig } from 'vitest/config'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  plugins: [vue()], // Permet de tester les composants Vue
+  plugins: [vue()],
   test: {
-    environment: 'jsdom', // Simule un navigateur pour les tests DOM
-    globals: true,        // Active les fonctions globales (describe, it, etc.)
+    environment: 'jsdom',
+    globals: true,
   },
 })
